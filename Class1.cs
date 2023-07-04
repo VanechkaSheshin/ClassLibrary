b3ec8eab4d70770332863767d070e0ae4ec4b9f5
Andrey S <306@306NEW-6>
2023-07-04 10:37:35 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+	public class Garage
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public List<Car> Cars = new List<Car>();
+
+		public Garage(string name, string address)
+		{
+			Name = name;
+			Address = address;
+		}
+
+		public void ShowAll()
+		{
+			Console.Write("Гараж:\n" +
+				$"\tНазвание: {Name}\n" +
+				$"\tАдрес: {Address}\n\n" +
+				"Список машин:\n");
+
+			Cars.Sort();
+			foreach (var car in Cars)
+			{
+				car.Show();
+			}
+		}
+	}
+
+	public class Car : IComparable<Car>
+	{
+		public string Owner { get; set; }
+		public string Number { get; set; }
+
+		public Car(string owner, string number)
+		{
+			Owner = owner;
+			Number = number;
+		}
+
+		public int CompareTo(Car other)
+		{
+			return Owner.CompareTo(other.Owner);
+		}
+
+		// Этот метод выводит информацию о машине
+		public void Show()
+		{
+			Console.WriteLine("Машина:\n" +
+				$"\tВладелец: {Owner}\n" +
+				$"\tНомер: {Number}");
+		}
+	}
+}
